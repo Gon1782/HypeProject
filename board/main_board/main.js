280622302010afabd56f8c74a0694b6e1742d0e4
YoungJae0910 <dbdudwo0910@gmail.com>
2022-11-22 00:30:35 +0900

    click another area

--- board/main_board/main.js
+++ board/main_board/main.js
@@ -32,3 +32,39 @@ $(document).ready(function(){
         $('.heart1').toggleClass("heart-active1")
     });
 });
+
+// =======================외부클릭시 지워짐
+document.addEventListener('click', function handleClickOutsideBox(event) {
+    // 👇️ the element the user clicked
+    console.log('user clicked: ', event.target);
+
+    const box = document.getElementById('search_history');
+    const button = document.getElementById('search_input')
+
+    if (!box.contains(event.target) && !button.contains(event.target) ) {
+        box.classList.toggle('hide_bar');
+    }
+});
+
+document.addEventListener('click', function handleClickOutsideBox(event) {
+    // 👇️ the element the user clicked
+    console.log('user clicked: ', event.target);
+
+    const box1 = document.getElementById('search_history1');
+    const button1 = document.getElementById('search_input1')
+
+    if (!box1.contains(event.target) && !button1.contains(event.target) ) {
+        box1.classList.toggle('hide_bar1');
+    }
+});
+
+
+//   && << 둘다 true일때 실행되라
+//    ||  << 둘중하나가 true라면 실행해라
+
+// const goboard = 
+
+// function next(event){
+//     event.preventDefault()
+//     handleLocation
+// }
